e10dc2e93c28204c0cdf531a2e09a6a4cd445d48
LeipreachanTeo <pokiboom@mail.ru>
2022-08-23 14:40:36 +0700

    Second task

---
+++ Two_task/two_task.cs
@@ -0,0 +1,16 @@
+﻿int rdm = new Random().Next(1, 65536);
+Console.WriteLine(rdm);
+int digit_index = 2;
+
+if (rdm > 99)
+{
+    int digit_third = rdm.ToString()[digit_index] - '0';
+    Console.WriteLine(digit_third);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
