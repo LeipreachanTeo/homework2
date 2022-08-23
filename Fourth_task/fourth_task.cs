9878e41ff5c87f615d561ebe79efd0d18717eb46
LeipreachanTeo <pokiboom@mail.ru>
2022-08-23 15:26:44 +0700

    fourth task

---
+++ Fourth_task/fourth_task.cs
@@ -0,0 +1,10 @@
+﻿int rdm = new Random().Next(1, 1000000);
+Console.WriteLine(rdm);
+if (rdm % 7 == 0 && rdm % 23 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Некратно");
+}
